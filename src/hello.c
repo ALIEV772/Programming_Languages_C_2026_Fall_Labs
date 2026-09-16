7c3126ec9af663a2e9ffcf4c29d504aee8aa0bf8
ALIEV772 <liyevli74@gmail.com>
2026-09-16 07:43:49 +0000

    update hello message

--- src/hello.c
+++ src/hello.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
   printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
-    printf("  arg[%d] = %s\n", i, argv[i]);
+    printf("Hello from <Ali Aliyev>! \n");
   }
   return 0;
 }
